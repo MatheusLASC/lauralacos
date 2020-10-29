b613793b37c739ecd3b8aef34e6d47c11d362521
MatheusLASC <matheuslima.tipi@gmail.com>
2020-10-29 15:48:38 -0300

    Inserts no Banco de Dados

--- Code BD - PHPMYADMIN/lauralacos.sql
+++ Code BD - PHPMYADMIN/lauralacos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 23-Out-2020 às 00:23
+-- Tempo de geração: 29-Out-2020 às 19:47
 -- Versão do servidor: 10.4.14-MariaDB
 -- versão do PHP: 7.4.9
 
@@ -33,6 +33,13 @@ CREATE TABLE `tb_categorias` (
   `cate_slug` varchar(50) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Extraindo dados da tabela `tb_categorias`
+--
+
+INSERT INTO `tb_categorias` (`cate_id`, `cate_nome`, `cate_slug`) VALUES
+(1, 'Pronta Entrega', 'prontaentrega');
+
 -- --------------------------------------------------------
 
 --
@@ -55,6 +62,13 @@ CREATE TABLE `tb_produtos` (
   `pro_ativo` char(1) COLLATE utf8_unicode_ci NOT NULL,
   `pro_frete_free` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Não'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `tb_produtos`
+--
+
+INSERT INTO `tb_produtos` (`pro_id`, `pro_categoria`, `pro_nome`, `pro_desc`, `pro_valor`, `pro_img`, `pro_slug`, `pro_estoque`, `pro_comprimento`, `pro_apliques`, `pro_cores`, `pro_prendedor`, `pro_ativo`, `pro_frete_free`) VALUES
+(1, 1, 'Laço Double Cor', '2 Laços com cores diferentes', 10.90, 'laco1', 'lacodouble', 2, 2.00, 1, 1, 1.50, '1', 'Não');
 
 --
 -- Índices para tabelas despejadas
@@ -86,7 +100,7 @@ ALTER TABLE `tb_categorias`
 -- AUTO_INCREMENT de tabela `tb_produtos`
 --
 ALTER TABLE `tb_produtos`
-  MODIFY `pro_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `pro_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
